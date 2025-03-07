5b69bd17a05ab58b77c7750ea61fa48aa77ada4a
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-03-07 10:33:38 +0500

    cors commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./mongodb/connect');
 const Job = require("./mongodb/models/jobsModel");
 
@@ -7,6 +8,7 @@ const app = express();
 connectDB(); // Initialize MongoDB connection
 
 app.use(express.json());
+app.use(cors());
 
 // Get all jobs
 app.get('/jobs', async (req, res) => {
